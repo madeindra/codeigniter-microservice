e260b2fe2c8a6f0678a26bb9a327a2a56cd4d44d
Made Indra Wira Pramana <miwpramana@gmail.com>
2019-10-17 16:17:58 +0700

    (tdd): enable product route

--- warehouse/application/config/routes.php
+++ warehouse/application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['api/1.0.0/product'] = 'product';
+$route['api/1.0.0/product/:any'] = 'product';
