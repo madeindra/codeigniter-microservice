17c4b16a2e3177b9b0ae89e733e9da76b3b846f4
Made Indra Wira Pramana <miwpramana@gmail.com>
2019-10-18 10:53:06 +0700

    tdd: add order reject test in Order_test

--- order/application/tests/controllers/Order_test.php
+++ order/application/tests/controllers/Order_test.php
@@ -388,4 +388,43 @@ class Order_test extends TestCase
 
     }
 
+    public function test_order_rejected(){
+        // mock model and library on tested class' constructor and mock model's function
+        $this->request->setCallable(
+            function ($CI) {
+                $model = $this->getDouble(
+                    'Order_model', [
+                        'getOrder' => [
+                            'id' => '1',
+                            'product_id' => '1',
+                            'quantity' => '5',
+                            'price' => '5000'
+                        ]
+                    ]
+                );
+                // use mocked model to be loaded
+                $CI->Order_model = $model;
+
+                $library = $this->getDouble(
+                    'Php_func', [
+                        'processCheckout' => NULL
+                    ]
+                );
+                // use mocked library to be loaded
+                $CI->php_func = $library;
+            }
+        );
+
+        // set request as JSON
+        $this->request->setHeader('Content-type', 'application/json');
+
+        // send request
+        $output = $this->request('POST', 'api/1.0.0/order/checkout/1');
+
+        // assert response code and message
+        $this->assertResponseCode(400);
+        $this->assertStringContainsStringIgnoringCase('FALSE', $output);
+
+    }
+
 }
