6f2977c933437e5bbb64bf110d13ca508d49382f
Made Indra Wira Pramana <miwpramana@gmail.com>
2019-10-21 08:45:35 +0700

    feat: add PUT/DELETE by order id Invoice

--- financial/application/controllers/Invoice.php
+++ financial/application/controllers/Invoice.php
@@ -170,4 +170,100 @@ class Invoice extends REST_Controller
         );
 
     }
+
+    // Order PUT method
+    public function order_put(){
+        $id = $this->uri->segment(5);
+
+        if($id === NULL){
+            return $this->response(
+                [
+                    'success' => FALSE,
+                    'message' => 'provide an id'
+                ],
+                REST_Controller::HTTP_BAD_REQUEST,
+                TRUE
+            );
+        }
+
+        $order_id = $this->put('order_id') ?? NULL;
+        $total = $this->put('total') ?? NULL;
+        $status = $this->put('status') ?? 'incomplete';
+        
+        $data =
+        [
+            'id' => $id,
+            'status' => $status
+        ];
+        
+
+        if ($order_id !== NULL) {
+            $data += ['order_id' => $order_id];
+        
+        }
+
+        if ($total !== NULL) {
+            $data += ['total' => $total];
+        
+        }
+
+        if ($this->Invoice_model->updateInvoiceByOrderId($data, $id) > 0) {
+            return $this->response(
+                [
+                    'success' => TRUE,
+                    'message' => 'invoice has been updated',
+                    'data' => $data
+                ],
+                REST_Controller::HTTP_OK,
+                TRUE
+            );
+        }
+
+        return $this->response(
+            [
+                'success' => FALSE,
+                'message' => 'failed to update invoice'
+            ],
+            REST_Controller::HTTP_BAD_REQUEST,
+            TRUE
+        );
+
+    }
+
+    // Order DELETE method
+    public function order_delete(){
+        $id = $this->uri->segment(5);
+
+        if($id === NULL){
+            return $this->response(
+                [
+                    'success' => FALSE,
+                    'message' => 'provide an id'
+                ],
+                REST_Controller::HTTP_BAD_REQUEST,
+                TRUE
+            );
+        }
+
+        if ($this->Invoice_model->deleteInvoiceByOrderId($id) > 0) {
+            return $this->response(
+                [
+                    'success' => TRUE,
+                    'message' => 'invoice has been deleted'
+                ],
+                REST_Controller::HTTP_OK,
+                TRUE
+            );
+        }
+
+        return $this->response(
+            [
+                'success' => FALSE,
+                'message' => 'failed to delete invoice'
+            ],
+            REST_Controller::HTTP_BAD_REQUEST,
+            TRUE
+        );
+
+    }
 }
